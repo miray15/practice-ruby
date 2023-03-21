e6d6a63a728a3acf57dc6b70b1c7cb91dc557a13
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-21 14:15:15 -0500

    owner of store directions

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+
+# C. Use hashes with symbols to represent the following scenario:
+# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+# C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
+# C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+
