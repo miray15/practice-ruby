02afc695694b46a70e3bd1a82c148fe4d53c97de
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-21 17:30:33 -0500

    add statements for each item

--- store_item.rb
+++ store_item.rb
@@ -8,12 +8,9 @@ sneaker = {:color => "red", :price => 299.89, :material => "ostridge feathers"}
 coat = {:color => "gold", :price => 301.89, :material => "lion's mane"}
 sweater = {:color => "grey", :price => 15, :material => "shedded dog fur"}
 
-p "#{sneaker[:color]} #{sneaker[:price]} #{sneaker[:material]}"
+p "The sneaker color is #{sneaker[:color]}, its price is #{sneaker[:price]}, and it's made of some intereresting fibers: #{sneaker[:material]}"
 
-p "#{coat[:color]} #{sneaker[:price]} #{coat[:material]}
+p "The coat color is #{coat[:color]}, the price is $#{coat[:price]}, and the material is the ever exotic #{coat[:material]}."
 
-
-
-
-
+p "The color of the sweater is #{coat[:color]}, its cost is $#{sweater[:price]}, and the material is recycled and #{sweater[:material]}"
 
